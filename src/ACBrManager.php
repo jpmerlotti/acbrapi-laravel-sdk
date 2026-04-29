6fa231df0bf764458a0b77b8a77b243c7f34e735
jpmerlotti <joaopedromerlotti@gmail.com>
2026-04-29 16:00:13 -0300

    Build: Add .gitignore and remove accidental vendor files

--- src/ACBrManager.php
+++ src/ACBrManager.php
@@ -2,12 +2,23 @@
 
 namespace ACBr\Laravel;
 
-use ACBr\Api\CepApi;
-use ACBr\Api\CnpjApi;
-use ACBr\Api\NfeApi;
-use ACBr\Api\NfceApi;
+use ACBrAPI\Api\CepApi;
+use ACBrAPI\Api\CnpjApi;
+use ACBrAPI\Api\ContaApi;
+use ACBrAPI\Api\CteApi;
+use ACBrAPI\Api\CteOsApi;
+use ACBrAPI\Api\DceApi;
+use ACBrAPI\Api\DebugApi;
+use ACBrAPI\Api\DistribuioNFEApi;
+use ACBrAPI\Api\EmailApi;
+use ACBrAPI\Api\EmpresaApi;
+use ACBrAPI\Api\MdfeApi;
+use ACBrAPI\Api\NfceApi;
+use ACBrAPI\Api\NfcomApi;
+use ACBrAPI\Api\NfeApi;
+use ACBrAPI\Api\NfseApi;
 use GuzzleHttp\Client;
-use ACBr\Configuration;
+use ACBrAPI\Configuration;
 
 class ACBrManager
 {
@@ -23,35 +34,85 @@ class ACBrManager
         $this->sdkConfig->setHost($config['endpoints'][$config['environment']] ?? $config['endpoints']['sandbox']);
     }
 
-    public function nfe(): NfeApi
+    protected function getClient(): Client
     {
-        return new NfeApi(
-            new Client(['timeout' => $this->config['timeout']]),
-            $this->sdkConfig
-        );
-    }
-
-    public function nfce(): NfceApi
-    {
-        return new NfceApi(
-            new Client(['timeout' => $this->config['timeout']]),
-            $this->sdkConfig
-        );
+        return new Client([
+            'timeout' => $this->config['timeout'],
+        ]);
     }
 
     public function cep(): CepApi
     {
-        return new CepApi(
-            new Client(['timeout' => $this->config['timeout']]),
-            $this->sdkConfig
-        );
+        return new CepApi($this->getClient(), $this->sdkConfig);
     }
 
     public function cnpj(): CnpjApi
     {
-        return new CnpjApi(
-            new Client(['timeout' => $this->config['timeout']]),
-            $this->sdkConfig
-        );
+        return new CnpjApi($this->getClient(), $this->sdkConfig);
+    }
+
+    public function conta(): ContaApi
+    {
+        return new ContaApi($this->getClient(), $this->sdkConfig);
+    }
+
+    public function cte(): CteApi
+    {
+        return new CteApi($this->getClient(), $this->sdkConfig);
+    }
+
+    public function cteOs(): CteOsApi
+    {
+        return new CteOsApi($this->getClient(), $this->sdkConfig);
+    }
+
+    public function dce(): DceApi
+    {
+        return new DceApi($this->getClient(), $this->sdkConfig);
+    }
+
+    public function debug(): DebugApi
+    {
+        return new DebugApi($this->getClient(), $this->sdkConfig);
+    }
+
+    public function distribuicaoNfe(): DistribuioNFEApi
+    {
+        return new DistribuioNFEApi($this->getClient(), $this->sdkConfig);
+    }
+
+    public function email(): EmailApi
+    {
+        return new EmailApi($this->getClient(), $this->sdkConfig);
+    }
+
+    public function empresa(): EmpresaApi
+    {
+        return new EmpresaApi($this->getClient(), $this->sdkConfig);
+    }
+
+    public function mdfe(): MdfeApi
+    {
+        return new MdfeApi($this->getClient(), $this->sdkConfig);
+    }
+
+    public function nfce(): NfceApi
+    {
+        return new NfceApi($this->getClient(), $this->sdkConfig);
+    }
+
+    public function nfcom(): NfcomApi
+    {
+        return new NfcomApi($this->getClient(), $this->sdkConfig);
+    }
+
+    public function nfe(): NfeApi
+    {
+        return new NfeApi($this->getClient(), $this->sdkConfig);
+    }
+
+    public function nfse(): NfseApi
+    {
+        return new NfseApi($this->getClient(), $this->sdkConfig);
     }
 }
--- src/Facades/ACBr.php
+++ src/Facades/ACBr.php
@@ -5,10 +5,21 @@ namespace ACBr\Laravel\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static \ACBr\Api\NfeApi nfe()
- * @method static \ACBr\Api\NfceApi nfce()
- * @method static \ACBr\Api\CepApi cep()
- * @method static \ACBr\Api\CnpjApi cnpj()
+ * @method static \ACBrAPI\Api\CepApi cep()
+ * @method static \ACBrAPI\Api\CnpjApi cnpj()
+ * @method static \ACBrAPI\Api\ContaApi conta()
+ * @method static \ACBrAPI\Api\CteApi cte()
+ * @method static \ACBrAPI\Api\CteOsApi cteOs()
+ * @method static \ACBrAPI\Api\DceApi dce()
+ * @method static \ACBrAPI\Api\DebugApi debug()
+ * @method static \ACBrAPI\Api\DistribuioNFEApi distribuicaoNfe()
+ * @method static \ACBrAPI\Api\EmailApi email()
+ * @method static \ACBrAPI\Api\EmpresaApi empresa()
+ * @method static \ACBrAPI\Api\MdfeApi mdfe()
+ * @method static \ACBrAPI\Api\NfceApi nfce()
+ * @method static \ACBrAPI\Api\NfcomApi nfcom()
+ * @method static \ACBrAPI\Api\NfeApi nfe()
+ * @method static \ACBrAPI\Api\NfseApi nfse()
  *
  * @see \ACBr\Laravel\ACBrManager
  */
